cb9b76f476dccc09ce2840c725e389eb4faea4a7
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-11-22 17:05:49 -0500

    added setup for deploy

--- test-twitter/testTwitter/settings.py
+++ test-twitter/testTwitter/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c0!6_y17kk8a8+#m(^e6=tc#&&v&um-#1#l1($o+r!0!=%om$@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,8 +119,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+import os
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
