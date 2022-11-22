13ffbc730362d40abf3fcb2eb99d683a63ff1be9
Luis Sanchez <luisanchezdh@gmail.com>
2022-11-22 15:02:02 -0500

    serializers

--- test-twitter/tweets/serializers.py
+++ test-twitter/tweets/serializers.py
@@ -1,1 +1,19 @@
 from rest_framework import serializers
+
+from.models import Post, Comment,User
+
+class PostSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Post
+    fields='__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=Comment
+    field='__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model=User
+    field='__all__'
+
