51e8f010a952406b82b85de2e18eb76710417986
Alexis Herrera <aeherrer71@gmail.com>
2022-11-28 20:35:18 -0500

    uncommented out cors_allowed_origins

--- test-twitter/testTwitter/settings.py
+++ test-twitter/testTwitter/settings.py
@@ -59,12 +59,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:8080",
-#     "http://127.0.0.1:9000",
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
